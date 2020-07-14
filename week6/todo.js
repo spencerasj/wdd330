37b1bc5e57114116bd9e1ce725b489e04c35d51e
spencerasj <spe19021@byui.edu>
2020-07-14 16:44:40 -0600

    creating folder and files for final project

--- week6/todo.js
+++ week6/todo.js
@@ -29,75 +29,79 @@ function newElement() {
   }
   document.getElementById("addText").value = "";
 };
-  document.getElementById("submit").addEventListener("click", newElement);
-  document.getElementById("addText").addEventListener("keyup", function (event) {
-      event.preventDefault();
-      if (event.keyCode === 13) {
-        document.getElementById("submit").click();
+document.getElementById("submit").addEventListener("click", newElement);
+document.getElementById("addText").addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("submit").click();
 
-      };
-  });
-  // ------------------Code for clear all button-------------------------
+  };
+});
+// ------------------Code for clear all button-------------------------
 function clear() {
   let deleteList = document.getElementById('tasks');
   while (deleteList.hasChildNodes()) {
     deleteList.removeChild(deleteList.firstChild);
     localStorage.clear();
-  }};
-  document.getElementById("clearAll").addEventListener('click', clear); 
-  //---------------Code for check item off list------------------------------------
-  
-  var list = document.querySelector('ul');
-  list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
+  }
+};
+document.getElementById("clearAll").addEventListener('click', clear);
+//---------------Code for check item off list------------------------------------
+function checkItem(event) {
+  //let list = document.querySelectorAll('ul');
+  let item = event.target;
+  if (item.target.tagName === 'LI') {
+    item.target.classList.toggle('checked');
+  } else {
+    false;
+  }
+};
+document.querySelector("ul").addEventListener('click', checkItem(event));
+//  CODE FROM THE READING------------------------------------
+/*'use strict'
+const form = document.forms[0];
+class Item {
+    constructor(name) {
+        this.name = name;
     }
-  }, false);
-      //  CODE FROM THE READING------------------------------------
-      /*'use strict'
-      const form = document.forms[0];
-      class Item {
-          constructor(name) {
-              this.name = name;
-          }
-      }
+}
 
-      const controller = {
-          watch(form) {
-              form.addEventListener('submit', (event) => {
-              event.preventDefault(); // prevent the form from being submitted
-              this.add(form.name.value);
-              }, false);
-          },
-          add(name) {
-              const item = new Item(name);
-              view.render(item);
-          }
-      };
+const controller = {
+    watch(form) {
+        form.addEventListener('submit', (event) => {
+        event.preventDefault(); // prevent the form from being submitted
+        this.add(form.name.value);
+        }, false);
+    },
+    add(name) {
+        const item = new Item(name);
+        view.render(item);
+    }
+};
 
-      const view = {
-          render(item) {
-              const list = document.getElementById('list');
-              const li = document.createElement('li');
-              li.innerHTML = item.name;
-              list.appendChild(li);
-              // reset the input field
-              form.name.value = '';
-          }
-      };
+const view = {
+    render(item) {
+        const list = document.getElementById('list');
+        const li = document.createElement('li');
+        li.innerHTML = item.name;
+        list.appendChild(li);
+        // reset the input field
+        form.name.value = '';
+    }
+};
 
-      controller.watch(form);*/
-      /*----------------------------------BROTHER THAYNE VIDEO HELPS--------------------------------------------*/
-      //todo.js
-      /*export class Todo {
-        constructor(content, completed) {
-          this.Id = Date.now();
-          this.Content = content,
-          this.Completed = completed
-        }
-      }
-      //import {Todo} from './todo.js';
-      let todo1 = new Todo("mow the lawn", false);
-      console.log(todo1);
+controller.watch(form);*/
+/*----------------------------------BROTHER THAYNE VIDEO HELPS--------------------------------------------*/
+//todo.js
+/*export class Todo {
+  constructor(content, completed) {
+    this.Id = Date.now();
+    this.Content = content,
+    this.Completed = completed
+  }
+}
+//import {Todo} from './todo.js';
+let todo1 = new Todo("mow the lawn", false);
+console.log(todo1);
 
-      toDoList = [toDo];*/
+toDoList = [toDo];*/
