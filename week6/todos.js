1bbdc08b1779a6dabd3123e831a354331e920b83
spencerasj <spe19021@byui.edu>
2020-07-16 15:20:13 -0600

    making local storage work hopefully

--- week6/todos.js
+++ week6/todos.js
@@ -11,6 +11,7 @@ document.getElementById("submit").addEventListener("click", function (event) {
     event.preventDefault();
     let todo = new Todo(content.value);
     todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
     console.log(todos);
     outputTodos();
 });
@@ -38,83 +39,79 @@ list.addEventListener('click', function (event) {
     console.log(event.target);
     if (event.target.tagName === 'LI') {
         event.target.classList.toggle('checked');
-    }
-}, false);
-
-document.querySelector('#tasks').addEventListener('click',
-    (e) => {
-        let id = e.target.id;
+        let id = event.target.id;
         let todo = todos.find(t => t.Id == id);
         if (todo) {
             todo.Completed = !todo.Completed;
         }
         console.log(todos);
     }
-);
+//--------------------------------------------------------code to delete item-------------------------------------------------
+    if (event.target.tagName == "I") {
+        let li = event.target.parentNode.parentNode;
+        console.log(li);
+        let id = li.id;
+        let todo = todos.find(t => t.Id == id);
+        if (id === todo);
+        let index = todos.indexOf(todo);
+        todos.splice(index, 1);
+        li.remove();
+        console.log(todos);
+    }
+}, false);
+
+// document.querySelector('#tasks').addEventListener('click',
+//     (e) => {
+//         let id = e.target.id;
+//         let todo = todos.find(t => t.Id == id);
+//         if (todo) {
+//             todo.Completed = !todo.Completed;
+//         }
+//         console.log(todos);
+//     }
+// );
 
 //--------------------------------------------------------code to delete item-------------------------------------------------
-document.querySelectorAll(".trash").forEach(item => {
-            item.addEventListener('click', function (event) {
-                let target = event.target;
-                console.log(target.parentNode.parent.Node.Id);
-                if (target.tagName == "I") {
-                   target.parentNode.parent.Node.Id.remove();
-                }
-                console.log(todos);
-            }
-            )
-        });
+// document.querySelectorAll(".trash").forEach(item => {
+//     item.addEventListener('click', function (event) {
+//         let target = event.target;
+//         console.log(target);
+//         if (target.tagName == "I") {
+//             target.parentNode.parentNode.Id.remove();
+//         }
+//         console.log(todos);
+//     })
+// });
 
-            // document.querySelector('#tasks').addEventListener('click',
-            //     (e) => {
-            //         let id = e.target.id;
-            //         let todo = todos.find(t => t.Id == id);
-            //         if (todo) {
-            //             todos.splice(todo);
-            //         }
-            //         console.log(todos);
-            //     }
-            // );
+// document.querySelector('#tasks').addEventListener('click',
+//     (e) => {
+//         let id = e.target.id;
+//         let todo = todos.find(t => t.Id == id);
+//         if (todo) {
+//             todos.splice(todo);
+//         }
+//         console.log(todos);
+//     }
+// );
+
+//const data = JSON.parse(localStorage.getItem('todos'))
 
 
 
+//----------------------------------------------------------code to clear all items from list-----------------------------------------
+// function clear() {
+//     let deleteList = document.getElementById('tasks');
+//     while (deleteList.hasChildNodes()) {
+//       deleteList.removeChild(deleteList.firstChild);
+//       localStorage.clear();
+//     }
+//   };
+//   document.getElementById("clearAll").addEventListener('click', clear);
+  
 
-            //myArray.find(x => x.id === '45').foo
-            // list.addEventListener('click', event => {
-            //     if (event.target.classList.contains("checked")) {
-            //         const itemKey = event.target.parentElement.dataset.key;
-            //         toggleDone(itemKey);
-            //     }
-            //     if (event.target.classList.contains("trash")) {
-            //         const itemKey = event.target.parentElement.dataset.key;
-            //         deleteTodo(itemKey)
-            //     }
-            // });
 
-            // function deleteTodo(key) {
-
-            //     const index = todoItems.findIndex(item => item.id === Number(key));
-
-            //     const todo = {
-            //         deleted: true,
-            //         ...todoItems[index]
-            //     };
-            //     // remove the todo item from the array by filtering it out
-            //     todoItems = todoItems.filter(item => item.id !== Number(key));
-            //     outputTodos(todo);
-            // }
-            // console.log(todos);
-
-            // function toggleDone(key) {
-            //     const index = todos.findIndex(todo => todo.Id === Number(key));
-            //     todos[index].Completed = !todos[index].Completed;
-            //     //renderTodo(todoItems[index]);
-            //     outputTodos(todos[index]);
-            // }
-            //----------------------------------------------------------code to clear all items from list-----------------------------------------
-
-            // const addTask = document.getElementById("submit");
-            // const removeTask = document.getElementById("trash");
-            // const complete = document.getElementById("complete");
-            // const undone = document.getElementById("unfinished");
-            // const clearList = document.getElementById("clearall");
+// const addTask = document.getElementById("submit");
+// const removeTask = document.getElementById("trash");
+// const complete = document.getElementById("complete");
+// const undone = document.getElementById("unfinished");
+// const clearList = document.getElementById("clearall");
