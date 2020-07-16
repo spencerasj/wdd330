b236040d0b799dc17e9fbbafe1cfafb186246f4e
spencerasj <spe19021@byui.edu>
2020-07-16 11:47:18 -0600

    got click to change boolean in object

--- week6/todos.js
+++ week6/todos.js
@@ -41,30 +41,80 @@ list.addEventListener('click', function (event) {
     }
 }, false);
 
+document.querySelector('#tasks').addEventListener('click',
+    (e) => {
+        let id = e.target.id;
+        let todo = todos.find(t => t.Id == id);
+        if (todo) {
+            todo.Completed = !todo.Completed;
+        }
+        console.log(todos);
+    }
+);
+
 //--------------------------------------------------------code to delete item-------------------------------------------------
 document.querySelectorAll(".trash").forEach(item => {
-    item.addEventListener('click', function (event) {
-        console.log(event.target);
-        if (event.target.tagName === 'I') {
-            event.target.parentElement.parentElement.remove();
-        }
-    }, false);
-})
+            item.addEventListener('click', function (event) {
+                let target = event.target;
+                console.log(target.parentNode.parent.Node.Id);
+                if (target.tagName == "I") {
+                   target.parentNode.parent.Node.Id.remove();
+                }
+                console.log(todos);
+            }
+            )
+        });
 
-//----------------------------------------------------------code to clear all items from list-----------------------------------------
-// let li = document.querySelectorAll("li");
-// let trash = document.createElement("button");
-// let i = document.createElement("i");
-// i.className += ("fa fa-trash-o fa-lg");
-// li.appendChild(t);
-// trash.appendChild(i);
-// trash.classList.add("trash");
-// trash.setAttribute("type", "submit");
-// li.appendChild(trash);
-// //trash.addEventListener('click', function() {
+            // document.querySelector('#tasks').addEventListener('click',
+            //     (e) => {
+            //         let id = e.target.id;
+            //         let todo = todos.find(t => t.Id == id);
+            //         if (todo) {
+            //             todos.splice(todo);
+            //         }
+            //         console.log(todos);
+            //     }
+            // );
 
-// const addTask = document.getElementById("submit");
-// const removeTask = document.getElementById("trash");
-// const complete = document.getElementById("complete");
-// const undone = document.getElementById("unfinished");
-// const clearList = document.getElementById("clearall");
+
+
+
+            //myArray.find(x => x.id === '45').foo
+            // list.addEventListener('click', event => {
+            //     if (event.target.classList.contains("checked")) {
+            //         const itemKey = event.target.parentElement.dataset.key;
+            //         toggleDone(itemKey);
+            //     }
+            //     if (event.target.classList.contains("trash")) {
+            //         const itemKey = event.target.parentElement.dataset.key;
+            //         deleteTodo(itemKey)
+            //     }
+            // });
+
+            // function deleteTodo(key) {
+
+            //     const index = todoItems.findIndex(item => item.id === Number(key));
+
+            //     const todo = {
+            //         deleted: true,
+            //         ...todoItems[index]
+            //     };
+            //     // remove the todo item from the array by filtering it out
+            //     todoItems = todoItems.filter(item => item.id !== Number(key));
+            //     outputTodos(todo);
+            // }
+            // console.log(todos);
+
+            // function toggleDone(key) {
+            //     const index = todos.findIndex(todo => todo.Id === Number(key));
+            //     todos[index].Completed = !todos[index].Completed;
+            //     //renderTodo(todoItems[index]);
+            //     outputTodos(todos[index]);
+            // }
+            //----------------------------------------------------------code to clear all items from list-----------------------------------------
+
+            // const addTask = document.getElementById("submit");
+            // const removeTask = document.getElementById("trash");
+            // const complete = document.getElementById("complete");
+            // const undone = document.getElementById("unfinished");
+            // const clearList = document.getElementById("clearall");
